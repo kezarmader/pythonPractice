b5af13a0fef586113310132b3171bc9a5ba6740d
Kezar Mader <Kezar_Mader@trimble.com>
2022-04-20 15:03:23 -0500

    check palindrome with 1 error

---
+++ checkPalindrome1error.py
@@ -0,0 +1,18 @@
+def checkPalindrome(s):
+    return helper(s, 0, len(s) - 1, 1)
+
+def helper(s, start, end, error):
+    if start == end:
+        return True
+
+    if start == end - 1 and s[start] == s[end]:
+        return True
+    
+    if s[start] == s[end]:
+        return helper(s, start + 1, end - 1, error)
+    elif error > 0:
+        return helper(s, start + 1, end, error - 1) or helper(s, start, end - 1, error - 1)
+    
+    return False
+
+print(checkPalindrome('abccbca'))
